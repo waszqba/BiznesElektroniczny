93fc60d7968fd2ffadfcf485e2443a76dbe92296
Jakub <kuba@wasak.pl>
2020-12-18 18:52:04 +0100

    feat(full-containerization): update init.sh, remove redundant images

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 mkdir shop/img
-cp resources/img/* shop/img/
+cp -u resources/img/* shop/img/
 docker-compose up -d
 sleep 15
-rm -rf shop/admin
-rm -rf shop/install
-echo "Initialization complete, have fun with prestaCrap!"
+rm -rf shop/admin shop/install
+cp -u scrapping/*.csv shop/admin024olpraq/import/
+echo "Initialization complete, let the game begin..."
