d02e3a659466a49f8c1f11cb6eea605e3ff3a6a2
Jakub <kuba@wasak.pl>
2021-01-10 20:24:51 +0100

    feat(deployment): finish the init.sh

--- init.sh
+++ init.sh
@@ -7,11 +7,11 @@ NC='\033[0m' # No Color
 mkdir shop/img
 mkdir db
 cp -u resources/img/* shop/img/
-echo -e "${RED}Wait till the database import is finished, then do:${NC}"
-echo "rm -rf shop/admin shop/install"
-echo "cp -u scrapping/*.csv shop/admin024olpraq/import/"
+echo -e "\n${RED}Wait till the database import is finished, press ^C\n\n${NC}"
 sleep 10
 docker-compose up
+docker-compose up -d
+sleep 15
 rm -rf shop/admin shop/install
 cp -u scrapping/*.csv shop/admin024olpraq/import/
-echo -e "${GREEN}Initialization complete, let the game begin...${NC}"
+echo -e "\n${GREEN}Initialization complete, let the game begin...${NC}"
