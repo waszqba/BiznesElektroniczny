91ec4b0b5563c6c237a501a4dc9f98a35683519e
Jakub <kuba@wasak.pl>
2021-01-10 18:39:32 +0100

    feat(deployment): enable container for db

--- init.sh
+++ init.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 mkdir shop/img
+mkdir db
 cp -u resources/img/* shop/img/
 docker-compose up -d
-sleep 15
+sleep 30
 rm -rf shop/admin shop/install
 cp -u scrapping/*.csv shop/admin024olpraq/import/
 echo "Initialization complete, let the game begin..."
