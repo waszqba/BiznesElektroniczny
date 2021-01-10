28d9cd602a8311f288133f52d9ae981ec35f6e3b
Jakub <kuba@wasak.pl>
2021-01-10 19:50:27 +0100

    feat(deployment): add some life to init.sh

--- init.sh
+++ init.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 mkdir shop/img
 mkdir db
 cp -u resources/img/* shop/img/
-echo "Wait till the database import is finished, then do:"
+echo -e "${RED}Wait till the database import is finished, then do:${NC}"
 echo "rm -rf shop/admin shop/install"
 echo "cp -u scrapping/*.csv shop/admin024olpraq/import/"
 sleep 10
 docker-compose up
 rm -rf shop/admin shop/install
 cp -u scrapping/*.csv shop/admin024olpraq/import/
-echo "Initialization complete, let the game begin..."
+echo -e "${GREEN}Initialization complete, let the game begin...${NC}"
