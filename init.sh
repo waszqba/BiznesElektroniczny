6a6ef2846483e0e8d864006b722bb0f13b0441cc
Jakub <kuba@wasak.pl>
2021-01-10 19:42:23 +0100

    feat(deployment): update init.sh for long db import

--- init.sh
+++ init.sh
@@ -3,8 +3,11 @@
 mkdir shop/img
 mkdir db
 cp -u resources/img/* shop/img/
-docker-compose up -d
-sleep 30
+echo "Wait till the database import is finished, then do:"
+echo "rm -rf shop/admin shop/install"
+echo "cp -u scrapping/*.csv shop/admin024olpraq/import/"
+sleep 10
+docker-compose up
 rm -rf shop/admin shop/install
 cp -u scrapping/*.csv shop/admin024olpraq/import/
 echo "Initialization complete, let the game begin..."
